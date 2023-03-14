dad9278c84848709c78f023efa74562a54d4e0d2
Matt <mat.hop@gmail.com>
2023-03-14 13:48:03 +0000

    ready to go

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from datetime import date, datetime, timedelta
 import calendar
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 sns.set_style('whitegrid')
 
 def create_n_random_dates(n = 100, years = 100):
@@ -24,15 +25,15 @@ def create_n_random_dates(n = 100, years = 100):
 big_dates = create_n_random_dates(n = 100)
 plt.title('histogram of birthdays of the week')
 plt.hist([x[3]+1 for x in big_dates], 7)
-plt.show()
-
-plt.title('histogram of birthdays of the month')
-plt.hist([x[0]+1 for x in big_dates], 31)
+plt.xlabel('day of the week')
+plt.ylabel('count')
 plt.show()
 
 plt.title('scatter plot of birthdays of the day, and month')
-plt.scatter(x = [x[0]+1 for x in big_dates], 
-            y = [x[1]+1 for x in big_dates])
+x = [x[0]+1 for x in big_dates]
+y = [x[1]+1 for x in big_dates]
+u, c = np.unique(np.c_[x,y], return_counts = True, axis = 0)
+plt.scatter(u[:,0], u[:,1], s = c*25)
 plt.xlabel('days of the month')
 plt.ylabel('months of the year')
 plt.show()
@@ -42,7 +43,8 @@ ax = fig.add_subplot(projection='3d')
 ax.set_title('scatter plot of birthdays of the day, month, year')
 ax.scatter([x[0]+1 for x in big_dates], 
             [x[1]+1 for x in big_dates],
-            [x[2] for x in big_dates])
+            [x[2] for x in big_dates],
+            s = 5)
 
 ax.set_xlabel('days of the month')
 ax.set_ylabel('months of the year')
